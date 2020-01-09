6a09c8dc61ffd830ab2407fa6bd103ff006393ed
MhShohel <mhshohel89@gmail.com>
2020-01-09 14:31:41 +0600

    Course LIst

--- elements/CoursesList.php
+++ elements/CoursesList.php
@@ -3,14 +3,66 @@ namespace Oxygen\TutorElements;
 
 class CoursesList extends \OxygenTutorElements {
 
+    private $query_params = array(
+            'course_id'            => '',
+            'exclude_ids'   => '',
+            'category'      => '',
+            'orderby'       => '',
+            'order'         => '',
+            'limit'         => '',
+    );
+
 	function name() {
 		return 'Courses List';
 	}
 
 
 	function render($options, $defaults, $content) {
+		$a = tutils()->array_only($options, array_keys($this->query_params));
 
-	    echo 'Course List';
+		$course_post_type = tutor()->course_post_type;
+
+		$default_args = array(
+			'post_type'     => $course_post_type,
+			'post_status'   => 'publish',
+			'course_id'     => '',
+			'exclude_ids'   => '',
+			'category'      => '',
+
+			'orderby'       => 'ID',
+			'order'         => 'DESC',
+			'limit'         => '6',
+		);
+
+		$a = array_merge($default_args, $a);
+
+		if ( ! empty($a['course_id'])){
+			$ids = (array) explode(',', $a['course_id']);
+			$a['post__in'] = $ids;
+		}
+
+		if ( ! empty($a['exclude_ids'])){
+			$exclude_ids = (array) explode(',', $a['exclude_ids']);
+			$a['post__not_in'] = $exclude_ids;
+		}
+		if ( ! empty($a['category'])){
+			$category = (array) explode(',', $a['category']);
+
+			$a['tax_query'] = array(
+				array(
+					'taxonomy' => 'course-category',
+					'field'    => 'term_id',
+					'terms'    => $category,
+					'operator' => 'IN',
+				),
+			);
+		}
+		$a['posts_per_page'] = (int) $a['limit'];
+
+		wp_reset_query();
+		query_posts($a);
+		include_once tutor_get_template('shortcode.tutor-course');
+		wp_reset_query();
 	}
 
 	
@@ -26,7 +78,7 @@ class CoursesList extends \OxygenTutorElements {
 			array(
 				"type" => 'textfield',
 				"name" => 'Include ID',
-				"slug" => 'id',
+				"slug" => 'course_id',
 			)
 		);
 
@@ -36,7 +88,7 @@ class CoursesList extends \OxygenTutorElements {
 			array(
 				"type" => 'textfield',
 				"name" => 'Excludes IDS',
-				"slug" => 'include_ids',
+				"slug" => 'exclude_ids',
 			)
 		);
 
