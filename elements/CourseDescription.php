227636cae3da931afcd41a8e0c579754247c52f2
MhShohel <mhshohel89@gmail.com>
2020-01-13 12:58:13 +0600

    Added course description

--- elements/CourseDescription.php
+++ elements/CourseDescription.php
@@ -4,33 +4,34 @@ namespace Oxygen\TutorElements;
 class CourseDescription extends \OxygenTutorElements {
 
 	function name() {
-        return 'Course Description';
-    }
+		return 'Course Description';
+	}
 
-    function tutor_button_place() {
-        return "single_course";
-    }
+	function tutor_button_place() {
+		return "single_course";
+	}
 
-    /* function icon() {
-        return plugin_dir_url(__FILE__) . 'assets/'.basename(__FILE__, '.php').'.svg';
-    } */
+	/* function icon() {
+		return plugin_dir_url(__FILE__) . 'assets/'.basename(__FILE__, '.php').'.svg';
+	} */
 
-    function render($options, $defaults, $content) {
-        global $post;
-        echo get_the_content(null, false, $post);
-        /* tutor_course_content();
-        echo "tutor description"; */
-    }
+	function render($options, $defaults, $content) {
+		wp_reset_postdata();
 
+		global $post;
 
-    function class_names() {
-        return array('tutor-course-description', 'oxy-tutor-element');
-    }
+		tutor_course_content();
+	}
 
 
-    function controls() {
+	function class_names() {
+		return array('tutor-course-description', 'oxy-tutor-element');
+	}
 
-    }
+
+	function controls() {
+
+	}
 
 }
 
