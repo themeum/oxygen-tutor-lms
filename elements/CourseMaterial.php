d3a6fdb24c6b236faa2085710f5c4958a5067280
Sk Saju <sksaaju@gmail.com>
2020-01-20 19:33:07 +0600

    add element style control

--- elements/CourseMaterial.php
+++ elements/CourseMaterial.php
@@ -26,7 +26,35 @@ class CourseMaterial extends \OxygenTutorElements {
 
 
     function controls() {
+        $selector = ".tutor-course-material-includes-wrap";
+        $items_selector = $selector." .tutor-course-target-audience-items";
 
+        $this->typographySection('Title', $selector.' h4.tutor-segment-title');
+        $content_section = $this->addControlSection("content", __("Content"), "assets/icon.png", $this);
+        $content_icon = $content_section->addControlSection("icon", __("Icon"), "assets/icon.png", $this);
+        $content_icon->addStyleControls(
+			array(
+				array(
+                	"name" => __('Size'),
+                	"selector" => $items_selector.' li:before',
+					"property" => 'font-size',
+                ),
+				array(
+                	"name" => __('Color'),
+                	"selector" => $items_selector.' li:before',
+					"property" => 'color',
+				)
+			)
+        );
+        $content_section->typographySection('Typography', $items_selector, $this);
+        //spacing
+        $content_spacing = $content_section->addControlSection("spacing", __("Spacing"), "assets/icon.png", $this);
+        $content_spacing->addPreset(
+            "padding",
+            "item_padding",
+            __("Item Paddings"),
+            $items_selector.' li'
+        );
     }
 
 }
