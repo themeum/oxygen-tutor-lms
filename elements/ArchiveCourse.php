80bd8a63ba1a88e76fde3f340b29099a2dda84f3
Sk Saju <sksaaju@gmail.com>
2020-01-28 19:52:08 +0600

    add benefit addon icon and archive course page control

--- elements/ArchiveCourse.php
+++ elements/ArchiveCourse.php
@@ -18,22 +18,139 @@ class ArchiveCourse extends \OxygenTutorElements {
 		/**
 		 * Start Tutor Template
 		 */
-
 		$post_type = get_query_var('post_type');
 		$course_category = get_query_var('course-category');
-
 		if ( ($post_type === $course_post_type || ! empty($course_category) )  && $wp_query->is_archive){
 			$template = otlms_get_template('archive-course');
 			include_once $template;
 		}
-
 		/**
 		 * End Tutor Template
 		 */
 	}
 
-	public function tutor_button_place() {
+	function tutor_button_place() {
 		return "archive";
+	}
+
+	function controls() {
+		$selector = '.tutor-courses-wrap';
+		/* Layout */
+		$layout_section = $this->addControlSection("layout", __("Layout"), "assets/icon.png", $this);
+		$layout_section->addPreset("padding", "container_padding", __("Container Padding"), $selector);
+		$layout_section->addStyleControls(
+			array(
+				array(
+                	"name" => __('Background'),
+                	"selector" => $selector,
+					"property" => 'background-color',
+                )
+			)
+		);
+
+		/* Filter section */
+		$filter_selector = $selector.' .tutor-course-filter-wrap';
+		$this->typographySection(__("Results Count"), $filter_selector.' .tutor-course-archive-results-wrap', $this);
+
+		$sorting_select = $this->addControlSection("sorting_select", __("Sorting Select"), "assets/icon.png", $this);
+        $sorting_select_selector = $filter_selector.' .tutor-course-archive-filters-wrap .tutor-course-filter-form select';
+        $sorting_select->addPreset( "padding", "sorting_select_padding", __("Select Padding"), $sorting_select_selector);
+        $sorting_select->typographySection(__("Typography"), $sorting_select_selector, $this);
+        $sorting_select->borderSection(__("Border"), $sorting_select_selector, $this);
+        $sorting_select->borderSection(__("Focus Border"), $sorting_select_selector.":focus", $this);
+        $sorting_select->boxShadowSection(__("Box Shadow"), $sorting_select_selector, $this);
+        $sorting_select->boxShadowSection(__("Focus Box Shadow"), $sorting_select_selector.":focus", $this);
+
+		$filter_section_spacing = $this->addControlSection("filter_spacing", __("Filter Spacing"), "assets/icon.png", $this);
+		$filter_section_spacing->addPreset("padding", "filter_padding", __("Padding"), $filter_selector);
+		$filter_section_spacing->addPreset("margin", "filter_margin", __("Margin"), $filter_selector);
+
+		/* Course grid */
+		$course_grid = $this->addControlSection("course_grid", __("Course Grid"), "assets/icon.png", $this);
+		$course_col_selector = $selector.' .tutor-course-col-3';
+		$course_grid_selector = $course_col_selector.' .tutor-course';
+
+		/* level bagde */
+		$level_badge = $course_grid->addControlSection("level_badge", __("Level Badge"), "assets/icon.png", $this);
+		$level_badge_selector = $course_grid_selector." .tutor-course-loop-header-meta .tutor-course-loop-level";
+		$level_badge->addStyleControls(
+			array(
+				array(
+					"selector" => $level_badge_selector,
+					"property" => 'background-color',
+				),
+				array(
+					"selector" => $level_badge_selector,
+					"property" => 'font-size',
+				),
+				array(
+					"selector" => $level_badge_selector,
+					"property" => 'font-family',
+				),
+				array(
+					"selector" => $level_badge_selector,
+					"property" => 'line-height',
+				),
+				array(
+					"selector" => $level_badge_selector,
+					"property" => 'border-radius',
+				),
+				array(
+					"selector" => $level_badge_selector,
+					"property" => 'text-transform',
+				)
+			)
+		);
+
+		/* wishlist icon */
+		$wishlist_icon = $course_grid->addControlSection("wishlist_icon", __("Wishlist Icon"), "assets/icon.png", $this);
+		$wishlist_icon_selector = $course_grid_selector." .tutor-course-loop-header-meta .tutor-course-wishlist a";
+		$wishlist_icon->addStyleControls(
+			array(
+				array(
+					"name" 		=> __('Color'),
+					"selector" 	=> $wishlist_icon_selector,
+					"property" 	=> 'color',
+				),
+				array(
+					"name" 		=> __('Size'),
+					"selector" 	=> $wishlist_icon_selector,
+					"property" 	=> 'font-size',
+				)
+			)
+		);
+
+		$loop_course_container = $course_grid_selector.' .tutor-loop-course-container';
+
+		/* Stars */
+		$stars_section = $course_grid->addControlSection("stars", __("Stars"), "assets/icon.png", $this);
+		$star_selector = $loop_course_container." .tutor-loop-rating-wrap .tutor-star-rating-group";
+        $stars_section->addStyleControls(
+        	array(
+        		array(
+					"name" 		=> __('Color'),
+					"selector" 	=> $star_selector,
+					"property" 	=> 'color',
+				),
+				array(
+					"name" 		=> __('Size'),
+					"selector" 	=> $star_selector,
+					"property" 	=> 'font-size',
+				)
+        	)
+        );
+
+		$course_grid->typographySection(__("Course Title"), $loop_course_container.' .tutor-course-loop-title h2 a', $this);
+		$course_grid->typographySection(__("Meta Info"), $loop_course_container.' .tutor-course-loop-meta', $this);
+		$course_grid->typographySection(__("Author Label"), $loop_course_container.' .tutor-single-course-author-name span, '.$loop_course_container.' .tutor-course-lising-category span', $this);
+		$course_grid->typographySection(__("Author Value"), $loop_course_container.' .tutor-single-course-author-name, '.$loop_course_container.' .tutor-course-lising-category', $this);
+		
+		$loop_course_container_footer = $course_grid_selector.' .tutor-loop-course-footer';
+		$course_grid->typographySection(__("Course Price"), $loop_course_container_footer.' .price, '.$loop_course_container_footer.' .price .woocommerce-Price-amount', $this);
+		$course_grid->typographySection(__("Cart Button"), $loop_course_container_footer.' .tutor-loop-cart-btn-wrap a', $this);
+
+		$course_grid->boxShadowSection(__("Box Shadow"), $course_grid_selector, $this);
+        $course_grid->boxShadowSection(__("Hover Box Shadow"), $course_grid_selector.":hover", $this);
 	}
 
 }
