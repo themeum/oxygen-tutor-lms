2aac2dc2e6de74c770c7b61144954d093e0dcbb7
Md Rashed Hossain <wprashed@icloud.com>
2022-06-28 16:25:13 +0600

    Update CourseBenefits.php

--- elements/CourseBenefits.php
+++ elements/CourseBenefits.php
@@ -16,46 +16,15 @@ class CourseBenefits extends \OxygenTutorElements {
 	}
 
     function render($options, $defaults, $content) {
-        tutor_course_benefits_html();
+		echo '<div class="tutor-course-benefits">';
+        	tutor_course_benefits_html();
+		echo '</div>';
     }
 
     function controls() {
-        $selector = ".tutor-course-benefits-wrap";
-		$items_selector = $selector." .tutor-course-benefits-items";
-        $this->typographySection(__('Title'), $selector.' .course-benefits-title h4', $this);
-        $content_section = $this->addControlSection("content", __("Content"), "assets/icon.png", $this);
-        $content_icon = $content_section->addControlSection("content_icon", __("Icon"), "assets/icon.png", $this);
-		$content_icon->addStyleControls(
-			array(
-				array(
-                	"name" => __('Size'),
-                	"selector" => $items_selector.' li:before',
-					"property" => 'font-size',
-                ),
-				array(
-                	"name" => __('Color'),
-                	"selector" => $items_selector.' li:before',
-					"property" => 'color',
-				)
-			)
-        );
-		$content_section->typographySection(__('Typography'), $items_selector, $this);
-		$content_spacing = $content_section->addControlSection("content_spacing", __("Spacing"), "assets/icon.png", $this);
-        $content_spacing->addPreset(
-            "padding",
-            "content_item_padding",
-            __("Items Padding"),
-            $items_selector.' li'
-		);
-		$content_spacing->addStyleControls(
-			array(
-				array(
-                	"name" => __('Line Height'),
-                	"selector" => $items_selector.' li',
-					"property" => 'line-height',
-                )
-			)
-        );
+        $selector = ".tutor-course-benefits";
+
+		
     }
 }
 
