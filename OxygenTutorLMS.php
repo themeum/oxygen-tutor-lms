d205e0215724edcf5cdf0a0c63aa9a430de19159
Md Rashed Hossain <wprashed@icloud.com>
2022-06-28 14:52:40 +0600

    Update OxygenTutorLMS.php

--- OxygenTutorLMS.php
+++ OxygenTutorLMS.php
@@ -75,7 +75,6 @@ final class OxygenTutorLMS{
 		include_once OTLMS_PATH.'elements/CourseAuthor.php';
 		include_once OTLMS_PATH.'elements/CourseShare.php';
 		include_once OTLMS_PATH.'elements/CourseCategories.php';
-		//include_once OTLMS_PATH.'elements/CourseDuration.php';
 		//include_once OTLMS_PATH.'elements/CourseTotalEnrolled.php';
 		//include_once OTLMS_PATH.'elements/CourseLastUpdate.php';
 		//include_once OTLMS_PATH.'elements/CourseStatus.php';
