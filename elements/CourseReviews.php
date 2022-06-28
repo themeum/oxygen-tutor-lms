a1841907338295691781a4abb8699a379923d5b2
Md Rashed Hossain <wprashed@icloud.com>
2022-06-28 16:43:04 +0600

    Update CourseReviews.php

--- elements/CourseReviews.php
+++ elements/CourseReviews.php
@@ -20,16 +20,15 @@ class CourseReviews extends \OxygenTutorElements {
     }
 
     function controls() {
-        $selector = ".tutor-course-reviews";
-        /* Title */
-        $this->typographySection(__("Title"), $selector.' .tutor-segment-title', $this);
+        $review_selector = $selector." .tutor-course-reviews";
+        $this->typographySection(__("Review Title"), '.tutor-course-reviews h3', $this);
 
         /* Review average section */
-        $review_avg_section_selector = $selector." .course-avg-rating-wrap";
+        $review_avg_section_selector = $review_selector." .tutor-review-summary";
         $review_avg_section = $this->addControlSection("rating_avg", __("Review Avg"), "assets/icon.png", $this);
-        $review_avg_section->typographySection(__("Rating Text"), $review_avg_section_selector.' .course-avg-rating', $this);
+        $review_avg_section->typographySection(__("Rating Number"), $review_avg_section_selector.' .tutor-review-summary-average-rating', $this);
         $review_avg_stars = $review_avg_section->addControlSection("review_avg_stars", __("Rating Stars"), "assets/icon.png", $this);
-        $review_avg_stars_selector = $review_avg_section_selector." .tutor-star-rating-group";
+        $review_avg_stars_selector = $review_avg_section_selector." .tutor-ratings-stars span";
         $review_avg_stars->addStyleControls(
             array(
                 array(
@@ -44,12 +43,12 @@ class CourseReviews extends \OxygenTutorElements {
                 )
             )
         );
-        $review_avg_section->typographySection(__("Total Count"), $review_avg_section_selector.' .tutor-course-avg-rating-total', $this);
-        $review_avg_section_count_meter = $review_avg_section_selector.' .course-ratings-count-meter-wrap';
-    
-        $review_avg_right_bar_selector = $review_avg_section_count_meter.' .rating-meter-bar-wrap';
-        $review_avg_right_bar_main_selector = $review_avg_right_bar_selector.' .rating-meter-bar';
-        $review_avg_right_bar_fill_selector = $review_avg_right_bar_main_selector.' .rating-meter-fill-bar';
+        $review_avg_section->typographySection(__("Total Count"), $review_avg_section_selector.' div.tutor-color-secondary', $this);
+
+        $review_avg_section_count_meter = $review_avg_section_selector.' .tutor-review-summary-ratings';
+        $review_avg_right_bar_selector = $review_avg_section_count_meter.' .tutor-col';
+        $review_avg_right_bar_main_selector = $review_avg_right_bar_selector.' .tutor-progress-bar.tutor-ratings-progress-bar';
+        $review_avg_right_bar_fill_selector = $review_avg_right_bar_main_selector.' .tutor-progress-value';
         $review_avg_right_bar = $review_avg_section->addControlSection("review_avg_right_bar_main", __("Right Rating Bar"), "assets/icon.png", $this);
         $review_avg_right_bar->addStyleControls(
             array(
@@ -83,7 +82,7 @@ class CourseReviews extends \OxygenTutorElements {
             $review_avg_right_bar_main_selector
         );
         
-        $review_avg_right_stars_selector = $review_avg_section_count_meter.' .rating-meter-col i';
+        $review_avg_right_stars_selector = $review_avg_section_count_meter.' .tutor-ratings-stars span';
         $review_avg_right_stars = $review_avg_section->addControlSection("review_avg_right_stars", __("Right Rating Stars"), "assets/icon.png", $this);
         $review_avg_right_stars->addStyleControls(
             array(
@@ -100,12 +99,12 @@ class CourseReviews extends \OxygenTutorElements {
             )
         );
 
-        $review_avg_section->typographySection(__("Right Rating Text"), $review_avg_section_count_meter.' .rating-meter-col', $this);
+        $review_avg_section->typographySection(__("Right Rating Text"), $review_avg_section_count_meter.' .tutor-ratings-average', $this);
 
         /* Review list section */
-        $review_list_section_selector = $selector." .tutor-course-reviews-list";
+        $review_list_section_selector = $review_selector." .tutor-card-list-item";
         $review_list_section = $this->addControlSection("rating_list", __("Review list"), "assets/icon.png", $this);
-        $reviewer_image_selector = $review_list_section_selector." .review-avatar span";
+        $reviewer_image_selector = $review_list_section_selector." .tutor-avatar";
         $reviewer_image = $review_list_section->addControlSection("image", __("Image"), "assets/icon.png", $this);
 		$reviewer_image->addStyleControls(
 			array(
@@ -131,11 +130,11 @@ class CourseReviews extends \OxygenTutorElements {
 				)
 			)
         );
-        $review_list_section->typographySection(__("Name"), $review_list_section_selector.' .review-time-name p:first-child a', $this);
-        $review_list_section->typographySection(__("Time"), $review_list_section_selector.' .review-time-name p.review-meta', $this);
+        $review_list_section->typographySection(__("Name"), $review_list_section_selector.' .tutor-reviewer-name, .tutor-reviewer-name a', $this);
+        $review_list_section->typographySection(__("Time"), $review_list_section_selector.' .tutor-reviewed-on', $this);
 
         $reviewer_stars = $review_list_section->addControlSection("reviewer_stars", __("Stars"), "assets/icon.png", $this);
-        $reviewer_stars_selector = $review_list_section_selector." .tutor-star-rating-group";
+        $reviewer_stars_selector = $review_list_section_selector." .tutor-ratings-stars span";
         $reviewer_stars->addStyleControls(
             array(
                 array(
@@ -150,14 +149,7 @@ class CourseReviews extends \OxygenTutorElements {
                 )
             )
         );
-        $review_list_section->typographySection(__("Content"), $review_list_section_selector.' .review-content p', $this);
-        $review_list_spacing = $review_list_section->addControlSection("review_list_spacing", __("Spacing"), "assets/icon.png", $this);
-        $review_list_spacing->addPreset(
-            "padding",
-            "review_list_padding",
-            __("Padding"),
-            $review_list_section_selector.' .tutor-review-individual-item'
-        );
+        $review_list_section->typographySection(__("Content"), $review_list_section_selector.' .tutor-review-comment', $this);
     }
 
 }
