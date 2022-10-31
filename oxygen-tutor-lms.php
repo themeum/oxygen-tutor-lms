9a891c0712b172cd6c9cd6b525964d7382c594a2
Md Rashed Hossain <wprashed@icloud.com>
2022-10-31 16:44:52 +0600

    Update oxygen-tutor-lms.php

--- oxygen-tutor-lms.php
+++ oxygen-tutor-lms.php
@@ -4,7 +4,7 @@ Plugin Name: Oxygen Tutor LMS
 Plugin URI: https://www.themeum.com/product/tutor-lms/
 Description: Oxygen Builder Integration - Tutor LMS plugin let's you to design your courses, lesson page by Oxygen Builder.
 Author: Themeum
-Version: 2.0.1
+Version: 2.0.3
 Author URI: http://themeum.com
 Requires at least: 5.3
 Tested up to: 6.0
